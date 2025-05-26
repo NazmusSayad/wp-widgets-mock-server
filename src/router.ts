cb73c5987b48eb3cea4cc532a59fe9c84ea4c05c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-05-26 13:22:44 +0600

    feat(router): update check-price endpoint to use random fees and simplify response

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { getAvailabilityData } from './controller/availability.controller'
 import { getExploreData } from './controller/explore.controller'
+import { randomInRange } from './utils'
 
 const router = Router()
 
@@ -54,45 +55,16 @@ router.get('/api/availability', async (req, res) => {
 
 router.get('/api/check-price', async (req, res) => {
   const prices = {
-    cleaning_fee: 350,
+    booking_fee: randomInRange(500, 100),
+    cleaning_fee: randomInRange(300, 500),
     damage_protection: 149,
-    booking_fee: 59.4,
     accommodation_tax: 91.58,
     cleaning_fees_tax: 32.38,
     damage_protection_tax: 13.78,
   }
 
-  const fees = {
-    rent: {
-      amount: 360,
-      label: '3 nights @ $120/night',
-    },
-
-    fees: [
-      {
-        label: 'Booking Fee',
-        amount: prices.booking_fee,
-      },
-      {
-        label: 'Damage Protection',
-        amount: prices.damage_protection,
-      },
-    ],
-
-    taxes: [
-      {
-        label: "Cleaning fee's tax",
-        amount: prices.cleaning_fees_tax,
-      },
-      {
-        label: "Damage Protection 's tax",
-        amount: prices.damage_protection_tax,
-      },
-    ],
-  }
-
   res.json({
-    result: { fees, prices },
+    result: prices,
     status: 'ok',
   })
 })
