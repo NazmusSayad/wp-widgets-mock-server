bdb775b920f038aa687fec81b3c11ca3e4c258ef
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-05-26 13:18:27 +0600

    feat(router): change routes to use 'get' method and add check-price endpoint

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import { getExploreData } from './controller/explore.controller'
 
 const router = Router()
 
-router.use('/api/explore', async (req, res) => {
+router.get('/api/explore', async (req, res) => {
   const result = await getExploreData()
   const page = parseInt(req.query.page as string) || 1
   const pageSize = parseInt(req.query.limit as string) || 30
@@ -28,7 +28,7 @@ router.use('/api/explore', async (req, res) => {
   })
 })
 
-router.use('/api/availability', async (req, res) => {
+router.get('/api/availability', async (req, res) => {
   const result = await getAvailabilityData()
   const page = parseInt(req.query.page as string) || 1
   const pageSize = parseInt(req.query.limit as string) || 30
@@ -49,6 +49,51 @@ router.use('/api/availability', async (req, res) => {
       hasNextPage: page < totalPages,
       hasPrevPage: page > 1,
     },
+  })
+})
+
+router.get('/api/check-price', async (req, res) => {
+  const prices = {
+    cleaning_fee: 350,
+    damage_protection: 149,
+    booking_fee: 59.4,
+    accommodation_tax: 91.58,
+    cleaning_fees_tax: 32.38,
+    damage_protection_tax: 13.78,
+  }
+
+  const fees = {
+    rent: {
+      amount: 360,
+      label: '3 nights @ $120/night',
+    },
+
+    fees: [
+      {
+        label: 'Booking Fee',
+        amount: prices.booking_fee,
+      },
+      {
+        label: 'Damage Protection',
+        amount: prices.damage_protection,
+      },
+    ],
+
+    taxes: [
+      {
+        label: "Cleaning fee's tax",
+        amount: prices.cleaning_fees_tax,
+      },
+      {
+        label: "Damage Protection 's tax",
+        amount: prices.damage_protection_tax,
+      },
+    ],
+  }
+
+  res.json({
+    result: { fees, prices },
+    status: 'ok',
   })
 })
 
